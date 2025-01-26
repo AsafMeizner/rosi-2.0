5990caf0749e2ccddd708d1c70136cc532b380bb
Asaf Meizner <asafmeizner@gmail.com>
2025-01-26 17:28:11 +0200

    improved tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,14 +5,14 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Navbar } from "../components/navbar";
 import { Footer } from "../components/footer";
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "ROSI | Robotics Open Source International",
-  description: "ROSI - Robotics Open Source International Is an Open Source project aimed at creating a common infrastructure among all FIRST teams internationally. You can upload presentations, CAD files, code scenes, photographed lectures and more. So that any team can learn from the other teams while sharing its knowledge When the situation the site strives to promote is raising the level of all teams internationally. In all topics, from the establishment of community and media projects, through software and code to Manufacturing and engineering.",
+  description: "ROSI - A platform for FIRST teams to share knowledge globally through presentations, CAD files, code, and more, raising team performance worldwide.",
 };
 
 export default function RootLayout({
@@ -23,13 +23,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>ROSI | Robotics Open Source International</title>
-        <meta name="description" content={metadata.description || "Default description"} />
+        <title>{String(metadata.title) ?? ''}</title>
+        <meta name="description" content={metadata.description ?? undefined} />
         <link rel="icon" href="/favicon.ico" />
 
         {/* Open Graph Meta Tags */}
-        <meta property="og:title" content={String(metadata.title) ?? ''} />
-        <meta property="og:description" content={metadata.description ?? ''} />
+        <meta property="og:title" content={String(metadata.title) || undefined} />
+        <meta property="og:description" content={String(metadata.description) || undefined} />
         <meta property="og:image" content="https://rosi.co.il/_next/image?url=%2Fimages%2Fdark_rosi.png&w=1200&q=75" />
         <meta property="og:url" content="https://rosi.co.il" />
         <meta property="og:type" content="website" />
@@ -37,16 +37,16 @@ export default function RootLayout({
 
         {/* Twitter Card Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={String(metadata.title) ?? ''} />
-        <meta name="twitter:description" content={metadata.description ?? ''} />
+        <meta name="twitter:title" content={String(metadata.title) ?? undefined} />
+        <meta name="twitter:description" content={metadata.description ?? undefined} />
         <meta name="twitter:image" content="https://rosi.co.il/_next/image?url=%2Fimages%2Fdark_rosi.png&w=1200&q=75" />
         <meta name="twitter:site" content="@YourTwitterHandle" /> {/* Replace with your Twitter handle */}
         <meta name="twitter:creator" content="@YourTwitterHandle" /> {/* Replace with your Twitter handle */}
       </head>
       <body className={inter.className}>
-        <ToastContainer style={{marginTop: "10vh"}}/>
+        <ToastContainer style={{ marginTop: "10vh" }} />
         <Analytics />
-        <SpeedInsights/>
+        <SpeedInsights />
         <Navbar />
         <div className="mt-10"></div>
         {children}
