2a597e2d0d9f554db8562928dcc597f926385c7f
Asaf Meizner <asafmeizner@gmail.com>
2025-05-15 17:30:07 +0300

    goodbye

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,9 +5,9 @@ export const Footer: React.FC = () => {
         <div className="bg-blue-50 dark:bg-slate-900 pt-10">
             <footer className="bg-white dark:bg-gray-800 text-gray-800 dark:text-white py-4">
                 <div className="max-w-screen-xl mx-auto px-4">
-                    <div className="flex flex-col justify-center items-center">
+                    <div className="flex justify-center items-center">
                         <div className="text-center">
-                            <div className="flex flex-col md:flex-row gap-4 justify-center items-center">
+                            <div className="flex flex-row md:flex-col gap-4 justify-center items-center">
                                 <div className="flex flex-col items-center">
                                     <img 
                                         src="/images/ma5951.png" 
@@ -18,14 +18,6 @@ export const Footer: React.FC = () => {
                                 </div>
                                 <div className="mt-4 text-center">
                                     <p className="text-sm">&copy; {new Date().getFullYear()} Robotics Open Source International- ROSI. All rights reserved.</p>
-                                </div>
-                                <div className="flex flex-col items-center">
-                                    <img 
-                                        src="/images/miscar1574.png" 
-                                        alt="MIS-CAR 1574 logo" 
-                                        className="h-40 w-40 mb-2" 
-                                    />
-                                    <span className="text-sm">MIS-CAR 1574</span>
                                 </div>
                             </div>
                         </div>
